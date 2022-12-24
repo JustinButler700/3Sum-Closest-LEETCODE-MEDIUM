5b5f7685bb364c721242e63b8d50679295852740
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2022-12-24 10:22:57 -0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int sum = nums[0]+nums[1]+nums[2]; //initial sum value
         int result = nums[0]+nums[1]+nums[2]; 
-        Arrays.sort(nums); // We sort it so we can init a Binary Search.
+        Arrays.sort(nums); // We sort it so we can init a Search.
         for(int i = 0; i < nums.length-2; i++)
         {
-            int left = i+1; // Pointers for our Bin search
+            int left = i+1; // Pointers for our search
             int right = nums.length-1;
             //Implement a Search for closest sum
             while(left < right)
